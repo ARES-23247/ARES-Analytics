9ac28aa074cc8445fc82982e5f9b1cb834fc71c3
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-06-27 22:27:29 -0400

    build: updated default Client ID to match aresfirst-portal project

--- app/src/main/kotlin/com/ares/analytics/viewmodel/OnboardingViewModel.kt
+++ app/src/main/kotlin/com/ares/analytics/viewmodel/OnboardingViewModel.kt
@@ -18,7 +18,7 @@ data class OnboardingState(
     val robotId: String = "",
     val league: League = League.FTC,
     val nt4Host: String = "192.168.43.1",
-    val googleClientId: String = "292383002428-vruiakhk7ioaeidvrpd9hrq02nrd05me.apps.googleusercontent.com",
+    val googleClientId: String = "205869391101-7bhkcpseglmtv0n3ig8i17e1ntl47tdr.apps.googleusercontent.com",
     val isVerifyingJava: Boolean = false,
     val javaEnvValid: Boolean? = null,
     val javaEnvMsg: String = "",
